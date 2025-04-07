b56a6da7b32a16ea9eb1da7b7e6b5790addbbe49
lamsworkshop <lamsworkshop@yahoo.com>
2025-04-07 13:55:20 +0800

    Update pizerocar.js

--- pizerocar.js
+++ pizerocar.js
@@ -1,35 +1,20 @@
-class ScratchFetch {
+class PiZeroCar {
     constructor() {
     }
     
     getInfo() {
         return {
-            "id": "Fetch",
-            "name": "Fetch",
+            "id": "PiZeroCar",
+            "name": "PiZeroCar",
             "blocks": [
                         {
-                            "opcode": "fetchURL",
+                            "opcode": "connect",
                             "blockType": "reporter",
-                            "text": "fetch data from [url]",
+                            "text": "connect to PiZeroCar @ ip: ",
                             "arguments": {
                                 "url": {
                                     "type": "string",
-                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
-                                },
-                            }
-                        },
-                        {
-                            "opcode": "jsonExtract",
-                            "blockType": "reporter",
-                            "text": "extract [name] from [data]",
-                            "arguments": {
-                                "name": {
-                                    "type": "string",
-                                    "defaultValue": "temperature"
-                                },
-                                "data": {
-                                    "type": "string",
-                                    "defaultValue": '{"temperature": 12.3}'
+                                    "defaultValue": "192.168.50.183"
                                 },
                             }
                         },
@@ -37,25 +22,10 @@ class ScratchFetch {
         };
     }
     
-    fetchURL({url}) {
-        return fetch(url).then(response => response.text())
+    connect({url}) {
+        return "http://"+url+":8765"
     }
     
-    jsonExtract({name,data}) {
-        var parsed = JSON.parse(data)
-        if (name in parsed) {
-            var out = parsed[name]
-            var t = typeof(out)
-            if (t == "string" || t == "number")
-                return out
-            if (t == "boolean")
-                return t ? 1 : 0
-            return JSON.stringify(out)
-        }
-        else {
-            return ""
-        }
-    }
 }
 
-Scratch.extensions.register(new ScratchFetch())
+Scratch.extensions.register(new PiZeroCar())
