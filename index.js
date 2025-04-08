aa8f2ad7cbeefcf3a982d2d7c7d7afb9c0d1a4d2
lamsworkshop <lamsworkshop@yahoo.com>
2025-04-08 19:51:27 +0800

    Update and rename pizerocar.js to index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+class PiZeroCar {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            id: 'PiZeroCar',
+            name: 'PiZeroCar',
+            blocks: [
+                        {
+                            opcode: 'connect',
+                            blockType: BlockType.COMMAND,
+                            text: 'connect to PiZeroCar',
+                        },
+                        {
+                            opcode: 'send',
+                            blockType: BlockType.COMMAND,
+                            text: 'send [msg]',
+                            arguments: {
+                                msg: {
+                                    type: ArgumentType.STRING,
+                                    defaultValue: 'hello'
+                                }
+                            }
+                        },
+                ],
+        };
+    }
+    
+    connect() {
+        this.WS = new WebSocket('ws://localhost:9001');
+    }
+
+    send({msg}) {
+        this.WS.send(msg);
+    }
+    
+}
+
+//Scratch.extensions.register(new PiZeroCar())
